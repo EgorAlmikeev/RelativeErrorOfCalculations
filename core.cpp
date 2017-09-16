b23dd2a01722d68dd15c3914edcd5bf04b49771e
York <york.git@gmail.com>
2017-09-16 10:17:28 +0300

    student coefficient auto calc

--- core.cpp
+++ core.cpp
@@ -7,8 +7,26 @@ Core::Core(QObject *parent) : QObject(parent)
 
 void Core::startCalculating(int n, int precision_number)
 {
-    if(n <= 10)
-        n -= 2;
+    int row, col;
+    col = precision_number;
 
-    double student_value = student_value_table[n][precision_number];
+    if(n <= 10)
+        row = n - 2;
+    else if(n > 10 && n < 20)
+    {
+        if(n < 20)
+            row = 12;
+        if(n < 18)
+            row = 11;
+        if(n < 16)
+            row = 10;
+        if(n < 14)
+            row = 9;
+        if(n < 12)
+            row = 8;
+    }
+    else if(n >= 20)
+        row = 13;
+
+    double student_value = student_value_table[row][col];
 }
